647da0ca14df8f2a7ce55bf270ffc53362df738b
Brane Petkoski <petkoskibrane@gmail.com>
2017-04-14 19:27:06 +0200

    Added All Done checkbox,added All,active and completed categories.

--- module.js
+++ module.js
@@ -7,7 +7,7 @@ var Notes = (function () {
     // ************************************************************
 
     if (notes.length > 0) {
-        showNotes();
+        showNotes(getActiveButton());
     }
 
 
@@ -17,14 +17,14 @@ var Notes = (function () {
             text: newNote,
             completed: false
         });
-        showNotes();
+        showNotes(getActiveButton());
         saveLocaly();
     }
 
-    function showNotes() {
+    function showNotes(completedValue) {
         $("#notes").empty();
 
-        for (var i = notes.length-1; i >= 0; i--) {
+        for (var i = notes.length - 1; i >= 0; i--) {
             var notediv = $("<div>").addClass("note");
 
             var noteSpan = $("<span>", {
@@ -47,19 +47,32 @@ var Notes = (function () {
                 checked: notes[i].completed ? true : false
             });
 
+            // ***************************************************************
+            //     We are showing only completed, active(not completed) or
+            //         all notes based on passed completedValue value    
+            // ***************************************************************    
+            if (notes[i].completed === completedValue || completedValue === "All") {
 
 
-            deleteButton.addClass("fa fa-trash-o");
+                deleteButton.addClass("fa fa-trash-o");
 
-            noteSpan.text(notes[i].text);
-            noteSpan.addClass("notespan");
-            noteSpan.attr("contenteditable", "true");
-            notediv.append(checkBox);
+                noteSpan.text(notes[i].text);
+                noteSpan.addClass("notespan");
+                noteSpan.attr("contenteditable", "true");
+                notediv.append(checkBox);
 
-            notediv.append(noteSpan);
+                notediv.append(noteSpan);
 
-            notediv.append(deleteButton);
-            $("#notes").append(notediv);
+                notediv.append(deleteButton);
+                $("#notes").append(notediv);
+
+            }
+            // ***************************************************************
+            //     We are seting All button(checkbox) every time because
+            //      we want button to be checked based on how many
+            //        notes are completed or not completed.
+            // ***************************************************************  
+            setAllButtonValue();
 
         }
     }
@@ -67,36 +80,104 @@ var Notes = (function () {
     function deleteNote(notePosition) {
         notes.splice(notePosition, 1);
         saveLocaly();
-        showNotes();
+        showNotes(getActiveButton());
     }
 
     function toogleCompleted(notePosition) {
-        console.log(notePosition);
+
         notes[notePosition].completed = notes[notePosition].completed ? false : true;
         saveLocaly();
 
         $("#" + notePosition).toggleClass("completed");
+        showNotes(getActiveButton());
+        
 
 
     }
 
+    function toggleAll(value) {
+
+        notes.forEach(function (element) {
+            element.completed = value;
+        }, this);
+
+        showNotes(getActiveButton());
+        saveLocaly();
+    }
+
     function editNote(notePosition, noteValue) {
-        console.log(notePosition + "   " + noteValue);
+
 
         notes[notePosition].text = noteValue;
         saveLocaly();
-        showNotes();
+        showNotes(getActiveButton());
     }
 
     function saveLocaly() {
         localStorage.Notes = JSON.stringify(notes);
 
     }
+
+    function setAllButtonValue() {
+
+        var checkedCheckboxs = getCheckedNotes();
+
+        if (checkedCheckboxs === notes.length) {
+            $("#selectAll").prop("checked", true);
+        } else {
+            $("#selectAll").prop("checked", false);
+        }
+    }
+
+    function getCheckedNotes() {
+        var completedNumber = 0;
+        notes.forEach(function (element) {
+            if (element.completed) {
+                completedNumber++;
+            }
+
+        });
+        return completedNumber;
+    }
+
+    function showCompleted() {
+        showNotes(getActiveButton());
+    }
+
+    function showActive() {
+        showNotes(getActiveButton());
+    }
+
+    function showAll() {
+        showNotes(getActiveButton());
+    }
+
+    function getActiveButton() {
+        var activeButtonid = $(".activeButton").attr("id");
+        var returnValue;
+        switch (activeButtonid) {
+            case "showAll":
+                returnValue = "All";
+                break;
+            case "ShowActive":
+                returnValue = false;
+                break;
+            case "ShowCompleted":
+                returnValue = true;
+                break;
+        }
+        return returnValue;
+    }
     return {
         addNote: addNote,
         showNotes: showNotes,
         deleteNote: deleteNote,
         toogleCompleted: toogleCompleted,
-        editNote: editNote
+        editNote: editNote,
+        toggleAll: toggleAll,
+        showCompleted: showCompleted,
+        showActive: showActive,
+        showAll: showAll
+
     };
 })();
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 var inputText = $("#input");
 var notes = $("#notes");
-
+var toggleAll = $("#selectAll");
+var AllActiveCompleteButtonsContainer = $("#buttons-All-Active-Completed-Container");
 
 inputText.keypress(function (e) {
     if (enterIsPressed(e)) {
@@ -27,6 +28,8 @@ notes.click(function (event) {
 
     }
 
+    
+
 });
 
 notes.focusout(function (event) {
@@ -51,6 +54,31 @@ notes.keypress(function (event) {
     }
 });
 
+AllActiveCompleteButtonsContainer.click(function (event) {
+    switch (event.target.id) {
+        case "showAll":
+            resetActiveClass(event.target);
+            Notes.showAll();
+            break;
+        case "ShowActive":
+            resetActiveClass(event.target);  
+            Notes.showActive();    
+            break;
+        case "ShowCompleted":
+            resetActiveClass(event.target);  
+            Notes.showCompleted();    
+            break;
+    }
+});
+
+toggleAll.click(function (event) {
+    
+    if ($(event.target).is(":checked")) {
+        Notes.toggleAll(true);
+    } else {
+        Notes.toggleAll(false);  
+    } 
+});
 
 
 function addNote(value) {
@@ -60,4 +88,9 @@ function addNote(value) {
 
 function enterIsPressed(event) {
     return event.which === 13 ? true : false;
+}
+
+function resetActiveClass(target) {
+       $(".activeButton").removeClass("activeButton");
+            $(target).addClass("activeButton");
 }
